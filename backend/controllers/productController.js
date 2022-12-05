b62ebd0a5dd2b161de92afe178b13f3f6c73fe01
Susnik20 <anikh725@gmail.com>
2022-12-05 19:50:17 +0530

    anik

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,7 +16,7 @@ exports.createProduct = cacheAsyncErrors(async (req, res, next) => {
 //Get All Product
 exports.getAllProducts = cacheAsyncErrors(async (req, res) => {
 
-    const resultPerPage = 5;
+    const resultPerPage = 8;
     const productCount = await Product.countDocuments();
     const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
     const products = await apiFeature.query;
---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Header from "./component/layout/Header/Header.js"
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import webFont from 'webfontloader'
+import Footer from "./component/layout/Footer/Footer.js"
+import Home from "./component/Home/Home.js"
+import {Routes} from "react-router-dom"
+import React from 'react';
+
+
+
+function App() {
+  React.useEffect(()=>{
+    webFont.load({
+      google:{
+        families:["Roboto","Droid Sans", "Chilanka"]
+      }
+    });
+  },[]);
+
+  return (
+    <Router>
+    <Header />
+    <Routes>
+    <Route path="/" element={<Home />}></Route>
+    </Routes>
+    <Footer />
+    </Router>
+  );
+};
+
+export default App;
